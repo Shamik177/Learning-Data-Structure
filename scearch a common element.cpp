8b3b5522dddfcdc08d86cbdb97094d830f4a913a
Shamik177 <136921558+Shamik177@users.noreply.github.com>
2023-08-08 23:55:11 +0600

    Add files via upload

---
+++ scearch a common element.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x=0;
+    int arr1 [6];
+    int arr2 [5];
+    int arr3[11];
+
+    cout<<"Input array 1: "<<endl;
+    for(int j=0; j<6; j++){
+        cin>>arr1[j];
+    }
+
+    cout<<"Input array 2: "<<endl;
+    for(int j=0; j<5; j++){
+        cin>>arr2[j];
+    }
+    for(int i=0; i<6; i++){
+        for(int j=0; j<5; j++){
+            if(arr1[i]==arr2[j]){
+                arr3[x]=arr1[i];
+                x++;
+            }
+        }
+    }
+    if(x!=0){
+        cout<<"Common Elements:";
+        for(int i=0; i<x; i++)
+            cout<<arr3[i]<<" ";
+    }
+    else{
+        cout<<"No common element!";
+    }
+
+return 0;
+}
